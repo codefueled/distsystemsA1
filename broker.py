7db56b098138419ffcbdd8debb86aecf616ff362
ayushsangari <ayush.sangari@gmail.com>
2020-02-04 22:24:27 -0600

    fixed broker

--- broker.py
+++ broker.py
@@ -3,31 +3,44 @@ from __future__ import unicode_literals
 import zmq
 import sys
 
-context = zmq.Context()
-sub_socket = context.socket(zmq.SUB)
-pub_socket = context.socket(zmq.PUB)
-current_topics = []
+class Broker:
+    def __init__(self, ip_add):
+        context = zmq.Context()
+        self.sub_socket = context.socket(zmq.SUB)
+        self.pub_socket = context.socket(zmq.PUB)
+        self.current_topics = []
+        full_add1 = "tcp://" + str(ip_add) + ":1234"
+        full_add2 = "tcp://" + str(ip_add) + ":5556"
 
+        # bind to ip/ports
+        self.sub_socket.bind(full_add1)
+        self.sub_socket.subscribe("")
+        self.pub_socket.bind(full_add2)
+
+    def run(self):
+        message = self.sub_socket.recv_string()
+        topic, info = message.split("||")
+        error_flag = False
+
+        if topic == "REGISTER":
+            error = False
+            for curr_topic in self.current_topics:
+                if info.startswith(curr_topic):
+                    print("Topic is too similar to topic of another publisher, choose another")
+                    error = True
+            if not error:
+                self.current_topics.append(info)
+                print("Received: %s" % message)
+                self.pub_socket.send_string(message)
+        else:
+            if topic in self.current_topics:
+                print("Received: %s" % message)
+                self.pub_socket.send_string(message)
+            else:
+                print("Please start over with a valid topic")
 
 if __name__ == '__main__':
     ip_add = sys.argv[1]
-    full_add1 = "tcp://" + str(ip_add) + ":1234"
-    full_add2 = "tcp://" + str(ip_add) + ":5556"
-    # bind to ip/ports
-    sub_socket.bind(full_add1)
-    sub_socket.subscribe("")
-    pub_socket.bind(full_add2)
-
+    broker = Broker(ip_add)
     while True:
-        message = sub_socket.recv_string()
-        print(message)
-        topic, info = message.split("||")
-        error_flag = False
-        for curr_topic in current_topics:
-            if topic.startswith(curr_topic):
-                print("Topic is too similar to topic of another publisher, choose another")
-                error_flag = True
-        if not error_flag:
-            current_topics.append(topic)
-            print("Received: %s" % message)
-            pub_socket.send_string(message)
+        broker.run()
